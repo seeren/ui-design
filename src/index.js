d769993a9c7bb95e80be4b26a526baf17effa7e4
Cyril <consultant@seeren.fr>
2017-12-30 00:11:14 +0100

    Add wave

--- src/index.js
+++ src/index.js
@@ -7,15 +7,17 @@
  *    /_//_//_//_//_//_/
  *
  * @author Cyril <consultant@seeren.fr>
- * @version 1.1.0
+ * @version 1.3.0
  */
 
 import { dialog } from "./dialog/index";
+import { wave } from "./wave/index";
 
 export let ui = ((() => {
 
     return global.ui = {
-        dialog: dialog
+        dialog: dialog,
+        wave: wave
     };
 
 })());
