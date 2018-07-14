4742ee3221624075782af48e3d2c4cef6c2607f0
Cyril <consultant@seeren.fr>
2018-07-14 19:50:01 +0200

    Update @version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /**
- * This file contain ui-design
+ * ui-design
  *     __
  *    / /__ __ __ __ __ __
  *   / // // // // // // /
@@ -7,17 +7,18 @@
  *    /_//_//_//_//_//_/
  *
  * @author Cyril <consultant@seeren.fr>
- * @version 2.1.0
+ * @version 2.1.2
+ * 
  */
 
 import { Waver } from "./wave/waver.wave";
 import { Dialog } from "./dialog/dialog.dialog";
 import { Parallaxer } from "./parallax/parallaxer.parallax";
 
-export let ui = ((() => {
+export let ui = (() => {
     return global.ui = {
         dialog: new Dialog,
         parallax: new Parallaxer,
         wave:  new Waver
     };
-})());
+})();
